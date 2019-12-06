e5c8afdfdaf942c321f3336a2cb05384f04376d9
maoyining <2044964754@qq.com>
2019-12-06 22:42:28 +0800

    成员也添加好了

--- rearend/api/controllers/project/find-one.js
+++ rearend/api/controllers/project/find-one.js
@@ -21,11 +21,24 @@ module.exports = {
 
   fn: async function (inputs) {
 
-    let project = await Project.findOne({id:inputs.id}); 
+    let project = await Project.findOne({id:inputs.id});
     let a =await User.findOne({id:project.leader});
     if(project.leader!==null){
       project.leaderName=a.username;
     }
+    let m = await UserProject.find({project:inputs.id,status:5});
+    let member = [];
+    for(let i in m){
+      let query = {
+        where: {
+          id: m[i].user,
+        },
+        select: ['id', 'username']
+      };
+      let v = await User.findOne(query);
+      member.push(v);
+    }
+    project.member=member;
     return project;
 
   }
