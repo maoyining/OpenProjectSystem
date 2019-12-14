25f40c007d296e4f0dac6d95eba86d20339c0e5c
maoyining <2044964754@qq.com>
2019-12-14 12:08:27 +0800

    111

--- rearend/api/controllers/task/find-one.js
+++ rearend/api/controllers/task/find-one.js
@@ -19,7 +19,7 @@ module.exports = {
   },
 
   fn: async function (inputs) {
-    let t = await Task.findOne({status:inputs.id});
+    let t = await Task.findOne({id:inputs.id});
     let p = await Project.findOne({id:t.project});
     let u = await User.findOne({id:t.fromTeacher});
     t.projectName = p.name;
