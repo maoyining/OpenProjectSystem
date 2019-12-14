a5847138a0c75d07c062fd32248a15fa07a2d78d
maoyining <2044964754@qq.com>
2019-12-14 15:12:08 +0800

    111

--- rearend/api/controllers/project/find-tasks.js
+++ rearend/api/controllers/project/find-tasks.js
@@ -25,9 +25,10 @@ module.exports = {
       let v = t[i];
       let p = await Project.findOne({id:t[i].project});
       let u = await User.findOne({id:t[i].fromTeacher});
-
+      let stu = await User.findOne({id:t[i].toStudent});
       v.projectName = p.name;
       v.fromTeacherName = u.username;
+      v.toStudentName = stu.username;
       task.push(v);
     }
     return task;
